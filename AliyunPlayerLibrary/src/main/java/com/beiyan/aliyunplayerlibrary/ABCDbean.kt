83a0029a106e6af0c5ce4b5ab888cb5b11f8a066
linux <linux2005@163.com>
2024-07-05 17:57:59 +0800

    22

---
+++ AliyunPlayerLibrary/src/main/java/com/beiyan/aliyunplayerlibrary/ABCDbean.kt
@@ -0,0 +1,10 @@
+package com.beiyan.aliyunplayerlibrary
+
+/**
+ * @author : C4_雍和
+ * 描述 :
+ * 主要功能 :
+ * 维护人员 : C4_雍和
+ * date : 2024/7/5 17:57
+ */
+class ABCDbean {}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "CoroutineDemo"
 include(":app")
+include(":AliyunPlayerLibrary")
